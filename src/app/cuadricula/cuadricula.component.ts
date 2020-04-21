9d7e50c5aac2f36b1340a62ec6f60cc899309107
JuanAnayaR <juanayar@poligran.edu.co>
2020-04-21 15:02:04 -0500

    Creación Componente cuadricula y traslado datos HTML

---
+++ src/app/cuadricula/cuadricula.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cuadricula',
+  templateUrl: './cuadricula.component.html',
+  styleUrls: ['./cuadricula.component.css']
+})
+export class CuadriculaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
